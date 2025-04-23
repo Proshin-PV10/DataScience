4d909bea8f0e98e5617e2ea5a8f283fde473b070
Proshin-PV10 <167138897+Proshin-PV10@users.noreply.github.com>
2025-04-23 20:38:25 +0300

    Add files via upload

---
+++ SpamDet/Program.cs
@@ -0,0 +1,13 @@
+﻿//Load sample data
+using SpamDet;
+
+//Load sample data
+var sampleData = new SD.ModelInput()
+{
+    Content = @"I HAVE A DATE ON SUNDAY WITH WILL!!",
+};
+
+//Load model and predict output
+var result = SD.Predict(sampleData);
+
+Console.WriteLine(result.PredictedLabel);
---
+++ SpamDet2/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using Test1;
+
+var sampleData = new MLModel1.ModelInput()
+{
+    Col1 = @"Hello,Mike
+",
+};
+var result = MLModel1.Predict(sampleData);
+Console.WriteLine(result.PredictedLabel);
